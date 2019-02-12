3e4de792998fdfc78cc9d828c533cffe9a49525e
David Ames <david.ames@canonical.com>
2019-02-12 15:56:49 -0800

    Update charm-helpers-hooks.yaml and sync ch
    Using the new version of the sync tool which removes the charmhelpers
    directory before syncing, run charm helpers sync to find any unexpected
    missing dependencies.
    
    Change-Id: Ifa240291b5176da8081fc37473016a8254ba9e08

--- hooks/charmhelpers/fetch/python/version.py
+++ hooks/charmhelpers/fetch/python/version.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 # Copyright 2014-2015 Canonical Limited.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,3 +14,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import sys
+
+__author__ = "Jorge Niedbalski <jorge.niedbalski@canonical.com>"
+
+
+def current_version():
+    """Current system python version"""
+    return sys.version_info
+
+
+def current_version_string():
+    """Current system python version as string major.minor.micro"""
+    return "{0}.{1}.{2}".format(sys.version_info.major,
+                                sys.version_info.minor,
+                                sys.version_info.micro)
